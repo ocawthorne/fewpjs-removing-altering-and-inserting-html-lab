2202028258140288387ab775c23f00e3360ad286
ocawthorne <ocawthorne@gmail.com>
2021-01-07 21:44:48 +0000

    removing-altering

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ document.querySelector('#main').remove()
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = "Name is the champion!";
-newHeader.className = "victory";
+// newHeader.className = "victory";
